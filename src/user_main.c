40cd65bbbad0042f414c8606b7f737873745baf8
Anthony Sepa <protectivedad@gmail.com>
2026-01-16 10:23:06 -0400

    deepsleep: fix led staying on

--- src/user_main.c
+++ src/user_main.c
@@ -718,14 +718,6 @@ float g_wifi_temperature = 0;
 static byte g_secondsSpentInLowMemoryWarning = 0;
 void Main_OnEverySecond()
 {
-#if ENABLE_DEEPSLEEP
-	if (g_bWantPinDeepSleep) {
-		g_bWantPinDeepSleep = 0;
-		PINS_BeginDeepSleepWithPinWakeUp(g_pinDeepSleepWakeUp);
-		return;
-	}
-#endif
-
 #if PLATFORM_W600 || PLATFORM_W800
 #define TimeOut_t xTimeOutType 
 #endif
@@ -1098,6 +1090,15 @@ void Main_OnEverySecond()
 	// else task memory doesn't get freed
 	rtos_delay_milliseconds(1);
 
+#if ENABLE_DEEPSLEEP
+	if (g_bWantPinDeepSleep) {
+		g_bWantPinDeepSleep = 0;
+// may be necessary to enusre led is off when asleep not sure yet
+//		PIN_set_wifi_led(0);
+		PINS_BeginDeepSleepWithPinWakeUp(g_pinDeepSleepWakeUp);
+		return;
+	}
+#endif
 }
 
 
